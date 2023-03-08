ca8b32c1ee7bd128c248a2f6b3171e65dcee1cf5
noppar4t <nopparat.a64@rsu.ac.th>
2023-03-08 16:29:25 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "157.245.59.56",
+  user: "u6406475",
+  password: "6406475",
+  database: "u6406475_DIT322",
+  port: 3366,
+});
+
+var app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.json({
+    status: "ok",
+    message: "Hello World",
+  });
+});
+
+app.get("/customer", function (req, res) {
+  connection.query("SELECT * FROM a1_customer", function (err, results) {
+    console.log(results);
+    res.json(results);
+  });
+});
+
+app.get("/wowen_shoes", function (req, res) {
+  connection.query("SELECT * FROM a1_women_shoes", function (err, results) {
+    console.log(results);
+    res.json(results);
+  });
+});
+
+app.get("/order", function (req, res) {
+  connection.query("SELECT * FROM a1_order", function (err, results) {
+    console.log(results);
+    res.json(results);
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server is started.");
+});
